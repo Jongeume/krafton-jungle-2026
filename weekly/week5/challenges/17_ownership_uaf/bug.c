b82a1756666d294e3f4249ef2c6f1ae415a1acc2
Jongeume <jongeume@gmail.com>
2026-09-23 16:06:20 +0900

    solve: [week5] Ownership Uaf
    Closes #69

--- weekly/week5/challenges/17_ownership_uaf/bug.c
+++ weekly/week5/challenges/17_ownership_uaf/bug.c
@@ -13,7 +13,7 @@
  * [예시 상황]
  *   채팅 서버
  *   hello를 발행하면, 수신함(inbox)에 넣어서 상대 앱에 푸시하고, 동시에 대화 이력(log)에도 같은 메시지를 남깁니다.
- * 
+ *
  * [증상]
  *   구독자가 배달받은 Msg 를 free 하는데, 브로커의 log[] 는 "같은 포인터"를 여전히
  *   들고 있다(소유권이 두 곳에 걸침). broker_shutdown() 이 log[] 를 순회하며 이미
@@ -44,71 +44,88 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
-    int   id;
-    char *body;      /* 힙 문자열 */
+typedef struct
+{
+    int id;
+    char *body; /* 힙 문자열 */
 } Msg;
 
 #define QCAP 16
-typedef struct {
-    Msg *inbox[QCAP];   int head, tail;      /* 원형 큐 */
-    Msg *log[QCAP];     int log_n;            /* 감사용: 같은 Msg 포인터를 보관 */
+typedef struct
+{
+    Msg *inbox[QCAP];
+    int head, tail; /* 원형 큐 */
+    Msg *log[QCAP];
+    int log_n; /* 감사용: 같은 Msg 포인터를 보관 */
 } Broker;
 
 typedef void (*Subscriber)(Msg *m);
 
-static Msg *msg_new(int id, const char *body) {
+static Msg *msg_new(int id, const char *body)
+{
     Msg *m = malloc(sizeof *m);
-    if (!m) exit(1);
+    if (!m)
+        exit(1);
     m->id = id;
     m->body = malloc(strlen(body) + 1);
-    if (!m->body) exit(1);
+    if (!m->body)
+        exit(1);
     strcpy(m->body, body);
     return m;
 }
 
-static void msg_free(Msg *m) {
+static void msg_free(Msg *m)
+{
     free(m->body);
     free(m);
 }
 
-static void publish(Broker *b, int id, const char *body) {
+static void publish(Broker *b, int id, const char *body)
+{
     Msg *m = msg_new(id, body);
     b->inbox[b->tail] = m;
     b->tail = (b->tail + 1) % QCAP;
-    b->log[b->log_n++] = m;              
+    b->log[b->log_n++] = m;
 }
 
-static void deliver(Broker *b, Subscriber sub) {
-    while (b->head != b->tail) {
+static void deliver(Broker *b, Subscriber sub)
+{
+    while (b->head != b->tail)
+    {
         Msg *m = b->inbox[b->head];
         b->head = (b->head + 1) % QCAP;
-        sub(m);                          
+        sub(m);
     }
 }
 
-static void on_message(Msg *m) {
+static void on_message(Msg *m)
+{
     printf("recv #%d: %s\n", m->id, m->body);
-    msg_free(m);                         
+    // msg_free(m);
 }
 
-static void broker_shutdown(Broker *b) {
-    for (int i = 0; i < b->log_n; i++) {
-        msg_free(b->log[i]);             
+static void broker_shutdown(Broker *b)
+{
+    for (int i = 0; i < b->log_n; i++)
+    {
+        msg_free(b->log[i]);
+        b->log[i] = NULL;
+        b->inbox[i] = NULL;
     }
     b->log_n = 0;
 }
 
-int main(void) {
-    Broker b = { .head = 0, .tail = 0, .log_n = 0 };
+int main(void)
+{
+    Broker b = {.head = 0, .tail = 0, .log_n = 0};
 
     publish(&b, 1, "hello");
     publish(&b, 2, "world");
     publish(&b, 3, "broker");
 
-    deliver(&b, on_message);             
+    deliver(&b, on_message);
 
-    broker_shutdown(&b);                 
+    broker_shutdown(&b);
     printf("done\n");
     return 0;
 }
